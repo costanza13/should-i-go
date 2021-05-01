a7baa998d21f5cd91e25b11e88df3b12faa3b442
Emcee <mjchambersjr@gmail.com>
2021-05-01 11:11:57 -0700

    Setup for initial team search
    Add structure for html modules
    Added working dropdown
    Added first module

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+
+var dropdowns = document.querySelectorAll('.dropdown-trigger')
+for (var i = 0; i < dropdowns.length; i++) {
+	M.Dropdown.init(dropdowns[i]);
+}
+
 /*
   Functionality needed:
   - handle user team input
